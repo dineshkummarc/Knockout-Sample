87ad20628a2becdc12aec3612cfc3de4bddb8764
Pieter van de Bruggen <pieter@puppetlabs.com>
2011-08-23 17:16:25 -0700

    Adding support for displaying individual Gists.

--- application.js
+++ application.js
@@ -1,16 +1,48 @@
-var gists = ko.observableArray();
+var viewModel = {
+  currentPage: ko.observable('index'),
+  gists: ko.observableArray(),
+  gist: { attributes: ko.observable({}) },
+};
 
-var updateGists = function() {
+viewModel.gist.desc = ko.dependentObservable(function() {
+  return this.attributes().description || 'Gist #' + this.attributes().id;
+}, viewModel.gist);
+
+function fetchGists() {
   $.ajax({
     url: 'https://api.github.com/gists',
     success: function(data) {
-      gists(data.map(function(gist) {
+      viewModel.gists(data.map(function(gist) {
         gist.description = gist.description || 'Gist #' + gist.id;
         return gist;
       }));
     },
   });
-};
+}
 
-updateGists();
-ko.applyBindings({ gists: gists }, $('#content ul')[0]);
+function fetchGist(id) {
+  $.ajax({
+    url: 'https://api.github.com/gists/' + id,
+    success: function(data) {
+      data.files = $.map(data.files, function(value) { return value });
+      viewModel.gist.attributes(data);
+      viewModel.gist.attrs = data;
+      ko.applyBindings(viewModel, $('#content > div')[0]);
+    },
+  });
+}
+
+function showIndex() {
+  viewModel.currentPage('index');
+}
+
+function showGist(id) {
+  viewModel.gist.attributes({});
+  viewModel.currentPage('gist');
+  fetchGist(id);
+}
+
+fetchGists();
+setInterval(fetchGists, 60000);
+
+ko.applyBindings(viewModel);
